80f2326db9352a7f025e021c0c3fddf78371a774
champ <champ.za2002@hotmail.com>
2023-03-28 20:28:43 +0700

    หน้า Edit menu เเก้ไขรูปได้เเล้ว

--- MiniProjectDatabase/form/MenuForm/EditMenu.Designer.cs
+++ MiniProjectDatabase/form/MenuForm/EditMenu.Designer.cs
@@ -30,44 +30,37 @@ namespace MiniProjectDatabase
         private void InitializeComponent()
         {
             this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.Address = new System.Windows.Forms.TextBox();
-            this.Tel = new System.Windows.Forms.TextBox();
             this.Lname = new System.Windows.Forms.TextBox();
             this.Fname = new System.Windows.Forms.TextBox();
             this.Emp_id = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chosefile_btn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.label5.Location = new System.Drawing.Point(12, 97);
+            this.label5.Location = new System.Drawing.Point(21, 109);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(67, 25);
             this.label5.TabIndex = 33;
             this.label5.Text = "Detail";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.label4.Location = new System.Drawing.Point(12, 134);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 25);
-            this.label4.TabIndex = 32;
-            this.label4.Text = "Picture";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.label3.Location = new System.Drawing.Point(12, 168);
+            this.label3.Location = new System.Drawing.Point(21, 179);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(60, 25);
             this.label3.TabIndex = 31;
@@ -77,7 +70,7 @@ namespace MiniProjectDatabase
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Location = new System.Drawing.Point(21, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(119, 25);
             this.label2.TabIndex = 30;
@@ -87,7 +80,7 @@ namespace MiniProjectDatabase
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new System.Drawing.Point(21, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 25);
             this.label1.TabIndex = 29;
@@ -96,23 +89,15 @@ namespace MiniProjectDatabase
             // Address
             // 
             this.Address.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.Address.Location = new System.Drawing.Point(136, 165);
+            this.Address.Location = new System.Drawing.Point(145, 176);
             this.Address.Name = "Address";
             this.Address.Size = new System.Drawing.Size(248, 31);
             this.Address.TabIndex = 28;
             // 
-            // Tel
-            // 
-            this.Tel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.Tel.Location = new System.Drawing.Point(136, 128);
-            this.Tel.Name = "Tel";
-            this.Tel.Size = new System.Drawing.Size(360, 31);
-            this.Tel.TabIndex = 27;
-            // 
             // Lname
             // 
             this.Lname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.Lname.Location = new System.Drawing.Point(136, 91);
+            this.Lname.Location = new System.Drawing.Point(145, 103);
             this.Lname.Name = "Lname";
             this.Lname.Size = new System.Drawing.Size(248, 31);
             this.Lname.TabIndex = 26;
@@ -120,7 +105,7 @@ namespace MiniProjectDatabase
             // Fname
             // 
             this.Fname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.Fname.Location = new System.Drawing.Point(136, 54);
+            this.Fname.Location = new System.Drawing.Point(145, 66);
             this.Fname.Name = "Fname";
             this.Fname.Size = new System.Drawing.Size(360, 31);
             this.Fname.TabIndex = 25;
@@ -128,7 +113,7 @@ namespace MiniProjectDatabase
             // Emp_id
             // 
             this.Emp_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.Emp_id.Location = new System.Drawing.Point(136, 17);
+            this.Emp_id.Location = new System.Drawing.Point(145, 29);
             this.Emp_id.Name = "Emp_id";
             this.Emp_id.Size = new System.Drawing.Size(248, 31);
             this.Emp_id.TabIndex = 24;
@@ -154,25 +139,66 @@ namespace MiniProjectDatabase
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::MiniProjectDatabase.Properties.Resources._360_F_296055218_RXc721N9fSYIz3sEV7QALYquMVP31jdJ;
+            this.pictureBox1.Location = new System.Drawing.Point(612, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 233);
+            this.pictureBox1.TabIndex = 37;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chosefile_btn
+            // 
+            this.chosefile_btn.Location = new System.Drawing.Point(612, 270);
+            this.chosefile_btn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chosefile_btn.Name = "chosefile_btn";
+            this.chosefile_btn.Size = new System.Drawing.Size(150, 36);
+            this.chosefile_btn.TabIndex = 36;
+            this.chosefile_btn.Text = "Chose Image";
+            this.chosefile_btn.UseVisualStyleBackColor = true;
+            this.chosefile_btn.Click += new System.EventHandler(this.chosefile_btn_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.Location = new System.Drawing.Point(21, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 25);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Price";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(145, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 31);
+            this.textBox1.TabIndex = 38;
+            // 
             // EditMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 320);
+            this.ClientSize = new System.Drawing.Size(938, 320);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chosefile_btn);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Address);
-            this.Controls.Add(this.Tel);
             this.Controls.Add(this.Lname);
             this.Controls.Add(this.Fname);
             this.Controls.Add(this.Emp_id);
             this.Name = "EditMenu";
             this.Text = "EditMenu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,16 +207,18 @@ namespace MiniProjectDatabase
         #endregion
 
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox Address;
-        private System.Windows.Forms.TextBox Tel;
         private System.Windows.Forms.TextBox Lname;
         private System.Windows.Forms.TextBox Fname;
         private System.Windows.Forms.TextBox Emp_id;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button chosefile_btn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- MiniProjectDatabase/form/MenuForm/EditMenu.cs
+++ MiniProjectDatabase/form/MenuForm/EditMenu.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using MiniProjectDatabase.asset.database;
 using System.Data.OracleClient;
 
@@ -14,6 +16,8 @@ namespace MiniProjectDatabase
 {
     public partial class EditMenu : Form
     {
+        byte[] byteImg;
+        string filename;
         database db = new database();
 
         public EditMenu(string idt)
@@ -34,8 +38,110 @@ namespace MiniProjectDatabase
                 Lname.Text = reader["DETAIL"].ToString();
                 Address.Text = reader["TYPE"].ToString();
             }
-
             reader.Close();
+        }
+
+        public void saveimage(string filename)
+        {
+            //Create a Folder in your Root directory on your solution.
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("กรุณาใส่รูปภาพ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                string projectPath = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + "\\";
+                string folderPath = "asset\\img\\";
+                string imagePath = projectPath + folderPath + filename;
+                MessageBox.Show($"รูปภาพเก็บไว้ที่ :: {imagePath}", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MemoryStream ms = new MemoryStream(byteImg);
+                System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+                img.Save(imagePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+
+
+        }
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, imageIn.RawFormat);
+                return ms.ToArray();
+            }
+        }
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+        private System.Drawing.Image resizeImage(System.Drawing.Image imgToResize, Size size)
+        {
+            //Get the image current width  
+            int sourceWidth = imgToResize.Width;
+            //Get the image current height  
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //Calulate  width with new desired size  
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //Calculate height with new desired size  
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //New Width  
+            int destWidth = (int)(sourceWidth * nPercent);
+            //New Height  
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height  
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (System.Drawing.Image)b;
+        }
+        private void chosefile_btn_Click_1(object sender, EventArgs e)
+        {
+            Bitmap img;
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                filename = openFileDialog1.SafeFileName;
+                string file = openFileDialog1.FileName;
+                pictureBox1.BackgroundImage = null;
+                try
+                {
+                    byteImg = System.IO.File.ReadAllBytes(file);
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream(byteImg);
+                    img = new Bitmap(byteArrayToImage(byteImg));
+                    Image IMG = resizeImage(img, new Size(269, 233));
+                    pictureBox1.Image = IMG;
+
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+            /* OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            DialogResult result = OpenFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string directoryName = Path.GetDirectoryName(OpenFileDialog1.FileName);
+                // directoryName now contains the path
+                MessageBox.Show(directoryName);
+            }
+           */
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -51,7 +157,8 @@ namespace MiniProjectDatabase
             {
                 OracleCommand orclcmd = new OracleCommand();
                 string temp_sql =  $"UPDATE ENVY_MENU SET MENUNAME = '{Fname.Text}', DETAIL = '{Lname.Text}'," +
-                    $"TYPE = '{Address.Text}' WHERE MENU_ID = '{Emp_id.Text}'";
+                    $"TYPE = '{Address.Text}', PICTURE = '{filename}' WHERE MENU_ID = '{Emp_id.Text}'";
+                saveimage(filename);
                 try
                 {
                     db.openconnect();
